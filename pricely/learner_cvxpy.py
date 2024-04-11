187cd6bd4b63f6b7bf6a0d468cf341de07e5fc0a
Hsieh, Chiao <hsieh.chiao.7k@kyoto-u.ac.jp>
2024-04-11 02:03:15 +0000

    Disable learning Lyapunov for exponential stability

--- pricely/learner_cvxpy.py
+++ pricely/learner_cvxpy.py
@@ -15,6 +15,8 @@ class QuadraticLearner(PLyapunovLearner):
         self._u_dim = u_dim
         self._tol = tol
         self._pd_mat = cp.Variable((x_dim, x_dim), name="P", PSD=True)
+        # FIXME: Need to pick different objectives in order to support exponential stability
+        self._lambda = cp.Parameter(name="λ", value=0.0)  # XXX: Change to a variable for exponential stability
 
     def fit_loop(self, x: NDArrayFloat, u: NDArrayFloat, y: NDArrayFloat, max_epochs: int=1, **kwargs) -> Sequence[float]:
         # constraints from samples
@@ -22,11 +24,20 @@ class QuadraticLearner(PLyapunovLearner):
         yPx = cp.sum(cp.multiply(yP, x), axis=1)
         constraints = [
             self._pd_mat - self._tol >> 0,  # ensure x^T Px > 0 for x != 0
-            yPx + cp.sum(x**2, axis=1) <= 0,
+            yPx + 0.5*self._lambda*cp.sum(x**2, axis=1) <= 0,
         ]
 
-        # The following objective has no justification but works better!
-        obj = cp.Minimize(cp.sum_squares(x + 0.5*yP))
+        # Maximize the distance that still ensures yj P x <= 0
+        ## L-2 norm: ||x - xj||^2 <= -2*xj P yj - ||P yj||^2 ==> x P yj <= 0
+        ## The above sufficient condition is discovered through the S-procedure.
+        ## Let r denote the distance from xj to the hyperplane x P yj = 0.
+        ## Because the origin is on the hyperplane and (P yj/||P yj||) is the normal vector,
+        ## we can derive r = -xj (P yj / ||P yj||) since xj P yj < 0.
+        ## Then, -2*xj P yj - ||P yj||^2 = 2r||P yj|| - ||P yj||^2 = r^2 - (r-||P yj||)^2 <= r^2
+        obj = cp.Minimize(cp.sum(2*yPx) + cp.sum_squares(yP))
+        ## L-inf norm: dj_max <= -xj P yj / sum(abs(P yj))
+        # See "An analytical solution to the minimum Lp-norm of a hyperplane"
+        # obj = cp.Maximize(cp.sum(-xPy / cp.sum(Py, axis= 1)))  # XXX Not supported by CVXPY
         prob = cp.Problem(obj, constraints)
 
         for solver in [cp.CVXOPT, cp.CLARABEL]:
@@ -51,7 +62,7 @@ class QuadraticLearner(PLyapunovLearner):
 
     def lya_decay_rate(self) -> float:
         # Inverse of Spectral Radius
-        eig_max_inv = 1.0 / np.abs(np.linalg.eigvalsh(self._pd_mat.value)).max()
+        eig_max_inv = self._lambda.value / np.abs(np.linalg.eigvalsh(self._pd_mat.value)).max()
         return eig_max_inv
 
     def ctrl_exprs(self, x_vars: Sequence[Variable]) -> Sequence[Expr]:
