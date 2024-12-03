993b0cd9826d46c4a9bf4968f239b30b87b8a640
Hsieh, Chiao <hsieh.chiao.7k@kyoto-u.ac.jp>
2024-12-03 09:44:14 +0000

    Calculate bounds on norms

--- scripts/plot_phaseportrait_2d.py
+++ scripts/plot_phaseportrait_2d.py
@@ -15,19 +15,20 @@ def main(mod, cand: Optional[PLyapunovCandidate], out_dir: Path):
         dx = mod.f_bbox(x).squeeze()
         return dx[0], dx[1]
 
-    XLIM = mod.X_NORM_UB*1.25
-    Plot2D = PhasePortrait2D(f_2d, [[-XLIM, XLIM], [-XLIM, XLIM]], Title="", MeshDim=9, xlabel=r"", ylabel=r"", color="binary_r", odeint_method="euler")
+    Plot2D = PhasePortrait2D(f_2d, 1.0625*mod.X_LIM.T, Title="", MeshDim=9, xlabel=r"", ylabel=r"", color="binary_r", odeint_method="euler")
     fig, ax = Plot2D.plot()
     ax.grid(False)
     ax.set_aspect("equal")
 
-    for r in [mod.X_NORM_UB]:
+    norm_lb = mod.X_NORM_LB if hasattr(mod, "X_NORM_LB") else np.sqrt(2)*mod.ABS_X_LB
+    norm_ub = mod.X_NORM_UB if hasattr(mod, "X_NORM_UB") else float(np.linalg.norm(mod.X_LIM[0]))
+
+    for r in [norm_lb, norm_ub]:
         ax.add_patch(Circle((0, 0), r, color='r', fill=False))
-        t = ax.text(r*np.cos(-2*np.pi/4), r*np.sin(-2*np.pi/4), f"{r}", horizontalalignment='center', verticalalignment='center')
-    ax.add_patch(Circle((0, 0), mod.X_NORM_LB, color='r', fill=False))
+        t = ax.text(r*np.cos(-2*np.pi/4), r*np.sin(-2*np.pi/4), f"{r:.2}", horizontalalignment='center', verticalalignment='center')
 
     angs = np.linspace(0, 2*np.pi, 1000)
-    x_values = mod.X_NORM_UB*np.column_stack((np.cos(angs), np.sin(angs)))
+    x_values = norm_ub*np.column_stack((np.cos(angs), np.sin(angs)))
 
     if hasattr(mod, "nnet_lya"):
         level_ub = np.min(mod.nnet_lya(x_values))
