c52bb538e729b3c56024fd1cfba655afefdc7321
Hsieh, Chiao <hsieh.chiao.7k@kyoto-u.ac.jp>
2025-02-26 11:25:07 +0900

    Add timeout on each benchmark

--- run_hscc2025_polys.py
+++ run_hscc2025_polys.py
@@ -1,4 +1,7 @@
+import  multiprocessing
+from multiprocessing.pool import ThreadPool
 import csv
+from math import nan
 import os
 
 # Disable all tqdm progress bars
@@ -11,6 +14,7 @@ from examples import \
 from scripts import run_cegus
 
 MAX_SAMPLES = 5*10**5
+TIMEOUT = 4*3600  # 4 hours
 
 polys_cases = [
     fossil_nonpoly0,
@@ -29,8 +33,18 @@ for mod in polys_cases:
     name = mod.__name__.split('.')[-1]
     print("\n")
     print(f" Benchmark: {name} ".center(run_cegus.NCOLS, "#"))
-    # Synthesize a Lyapunov function
-    stats = run_cegus.execute(mod, out_dir=None, max_num_samples=MAX_SAMPLES)
+    try:
+        with ThreadPool() as pool:
+            # Synthesize a Lyapunov function
+            stats = pool.apply_async(
+                run_cegus.execute, (mod,), {"out_dir": None, "max_num_samples": MAX_SAMPLES}).get(timeout=TIMEOUT)
+    except multiprocessing.TimeoutError:
+        stats = run_cegus.Stats(
+            cegus_status="TIME_LIMIT", cegus_time_usage=nan,
+            last_epoch=-1, last_candidate=None,
+            num_samples_learn=-1,num_samples=-1,  num_regions=-1)
+        print(f"Reach time timit: {TIMEOUT} seconds")
+
     res_list.append((name, mod.X_NORM_UB, stats))
 
 with open('out/hscc2025_polys.csv', mode='w') as f:
